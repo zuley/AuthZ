6b5794e64ebdd37e303e54bf1030e4c854344b2a
zuley <914230482@qq.com>
2024-11-29 18:38:23 +0800

    feat：添加自动路由

--- tauri-app/vite.config.ts
+++ tauri-app/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import VueRouter from 'unplugin-vue-router/vite'
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [vue(), VueRouter()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
